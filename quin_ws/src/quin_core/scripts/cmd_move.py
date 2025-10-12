885c9eac39016ae8f827605772f1e9e3ebd0ea13
Earn <Earn@naja.com>
2025-10-12 16:34:36 +0700

    degree

--- quin_ws/src/quin_core/scripts/cmd_move.py
+++ quin_ws/src/quin_core/scripts/cmd_move.py
@@ -203,12 +203,8 @@ class Cmd_vel_to_motor_speed(Node):
     def cmd_gripper(self, msg):
         if msg.linear.x == 0:               # Closed Servo
             self.servo_angle = float(0.0)
-<<<<<<< HEAD
-=======
-
->>>>>>> 868a21c (system)
         if msg.linear.x == 1:               # Opened Servo
-            self.servo_angle = float(60.0)
+            self.servo_angle = float(50.0)
 
     def drill_feedback(self, msg: Twist):
         # unchanged placeholder
